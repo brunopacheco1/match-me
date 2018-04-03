2d64647ade988f772860fcb945a6744c8e0c630b
Bruno Silva <bruno_dasilva@epam.com>
2018-04-03 14:53:30 +0200

    Removing unnecessary code.

--- index.js
+++ index.js
@@ -3,13 +3,7 @@ let app = require("./app/init")();
 const config = app.profile;
 
 app.listen(config.port, () => {
-    let host = config.elastic.host;
-    
-    if(config.elastic.host.includes("@")) {
-        host = config.elastic.host.split("@")[1];
-    }
-
     console.log(`Server environment: ${config.profile}`);
     console.log(`Server port: ${config.port}`);
-    console.log(`Elastic Search address: ${host}:${config.elastic.port}`);
+    console.log(`Elastic Search address: ${config.elastic.host}:${config.elastic.port}`);
 });
