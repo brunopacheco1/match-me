d05065373451a1307731a41df70431c4c4e095d7
Bruno Silva <bruno_dasilva@epam.com>
2018-04-03 16:56:15 +0200

    Fixing elastic address log.

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const config = app.profile;
 app.listen(config.port, () => {
     console.log(`Server environment: ${config.profile}`);
     console.log(`Server port: ${config.port}`);
-    console.log(`Elastic Search address: ${config.elastic.host}:${config.elastic.port}`);
+    console.log(`Elastic Search address: ${config.elastic.host}`);
 });
